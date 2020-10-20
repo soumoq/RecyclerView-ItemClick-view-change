a02ab002893b01738b44ecf0829483d1cc28ed64
root <soumojyotiganguly270@gmail.com>
2020-10-20 15:12:04 +0530

    init

---
+++ app/src/main/java/com/example/itemclickrecyclerview/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.itemclickrecyclerview;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+    }
+}
